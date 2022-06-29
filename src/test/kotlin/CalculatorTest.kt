8251f5039dc556620cf8ae3bd3c1e6a8580b8ebd
CIANDT\caiquebrener <brenercaique0806@gmail.com>
2022-06-28 23:57:20 -0300

    Implementation Test

--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,3 +1,21 @@
-import org.junit.Assert.*
+import Calculator.Calculator
+import kotlin.reflect.typeOf
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
-class CalculatorTest
+class CalculatorTest {
+
+
+    @Test
+    fun `Should multiply 2 by 2`() {
+
+        val calculator = Calculator()
+
+        val result = calculator.parse("2 * 2")
+
+        assertEquals(4, result)
+
+    }
+
+
+}
